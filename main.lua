da98d240518b868d1b6500336dc1456bb39e9208
Alan Fernandez <alan.f@msn.com>
2023-04-08 11:49:50 -0300

    using nearest filter mode

--- main.lua
+++ main.lua
@@ -8,6 +8,7 @@ VIRTUAL_WIDTH = 432
 VIRTUAL_HEIGHT = 243
 
 function love.load()
+    love.graphics.setDefaultFilter('nearest', 'nearest')
     push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false,
         resizable = false,
